7932b7ab550b882c15a7f97a941584b4d85ae4d6
iamshs <mdsholayman998@gmail.com>
2022-04-17 00:36:00 +0600

    route-setup-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Blogs from './components/Pages/Blogs/Blogs';
+import AboutMe from './components/Pages/AboutMe/AboutMe';
+import RequireAuth from './components/Auth/RequireAuth/RequireAuth';
+import CheckOut from './components/Pages/CheckOut/CheckOut';
+import Login from './components/Auth/Login/Login';
+import Signup from './components/Auth/Signup/Signup';
+import NotFound from './components/Pages/NotFound/NotFound';
+import Home from './components/Pages/Home/Home';
+import Header from './components/Shared/Header/Header';
+import Footer from './components/Shared/Footer/Footer';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/blogs' element={<Blogs></Blogs>}/>
+        <Route path='/aboutme' element={<AboutMe></AboutMe>}/>
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <CheckOut></CheckOut>
+          </RequireAuth>
+        } />
+        <Route path='/login' element={<Login></Login>} />
+        <Route path='/signup' element={<Signup></Signup>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
