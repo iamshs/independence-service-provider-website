d35bf3584227d5337711061943d052fc6404fd6b
iamshs <mdsholayman998@gmail.com>
2022-04-17 22:15:54 +0600

    login-inputfield-initialize

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
+
+   const [userInfo,setUserInfo]=useState({
+       email:"",
+       pass:""
+   })
+   const [errors,setErrors]=useState({
+       email:'',
+       pass:'',
+       others:""
+   })
+  
+   const handleEmailChange = e =>{
+
+   }
+
+   const handlePasswordChange = e =>{
+
+   }
+
+   const handleLogin = e =>{
+       e.preventDefault()
+   }
+
     return (
-        <div>
-           <h1>Login here</h1> 
+        <div className="login-container">
+            <div className="login-title">LOGIN</div>
+            <form className="login-form" onSubmit={handleLogin}>
+                <input type="text" placeholder="Your Email" onChange={handleEmailChange} />
+               
+                <input type="password" placeholder="password" onChange={handlePasswordChange} />
+               
+                <button>Login</button>
+
+                <p>Don't have an account? <Link to="/signup">Sign up first</Link> </p>
+            </form>
+
+            <button >Google</button>
         </div>
     );
 };
